ffb455eb76abd293eca53801cb859d8442c730cb
Jay <jayshar@cisco.com>
2018-09-12 16:33:26 -0700

    adding ansible playbook for smart licensing feature

---
+++ 16.9.1/smart_licensing/verify_sl.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2018  Jay Sharma <jayshar@cisco.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# This script verifies status of smart license registration
+
+from pprint import pprint 
+import re
+import sys
+from argparse import ArgumentParser
+
+def parse_sl_out(output, status='registered'):
+	reg_status = re.findall(r'Registration:\s*Status:\s(\w+)', output, re.MULTILINE)
+	if reg_status[0].upper() == status.upper():
+		return_val = True
+	else:
+		return_val = False
+
+	return return_val
+
+if __name__ == '__main__':
+	parser = ArgumentParser(description='Select options.')
+	# Input parameters
+	parser.add_argument('--out', type=str, required=True,
+	                    help="The output")
+	args = parser.parse_args()
+	stat = (parse_sl_out(args.out.replace("\\n", "\n")))
+	print(stat)
