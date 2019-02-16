fcc4b9d43f6e7970f63ab14aee4ad6a700729ccf
Jay Sharma <40582054+jayshar@users.noreply.github.com>
2019-02-15 17:19:51 -0800

    Update sl_token.py

--- 16.10.1/smartlicensing_api/sl_token.py
+++ 16.10.1/smartlicensing_api/sl_token.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2018, Cisco and/or its affiliates
+# Author: Jay Sharma
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
 # ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
